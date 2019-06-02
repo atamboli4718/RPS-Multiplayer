6932ab2a2e3d39b664eb54b530f5d0d27a2dc4fd
Alicia Tamboli <atamboli4718@gmail.com>
2019-06-01 22:33:02 -0400

    finsihing up HTML

---
+++ assets/JS.js
@@ -0,0 +1,16 @@
+/* 
+Psuedo Code:
+Add the images of the rock paper scissors to the screen HTML
+Create the name & wins field for the HTML
+Create the box for the "game" and who won HTML
+Create Firebase project
+Create alert for Enter your name
+Write that data to the screen
+Create when entered assigned to 1 if empty, if not assign to two
+Assign name for chat window
+Create chat window with name and time and date stamp example example2
+Do logic for what beats what - find project from Week 4
+Playzer 1 always goes first
+Winner in center box
+
+*/
