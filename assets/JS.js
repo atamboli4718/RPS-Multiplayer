f6de4cbf706c68882d142c1870351b41b9e6276c
Alicia Tamboli <atamboli4718@gmail.com>
2019-06-03 19:41:35 -0400

    added sections for the player data and brushed up the CSS. also added the firebase links and started entering names to the database.

--- assets/JS.js
+++ assets/JS.js
@@ -14,3 +14,59 @@ Playzer 1 always goes first
 Winner in center box
 
 */
+
+// Firebase objects and initilization
+var firebaseConfig = {
+    apiKey: "AIzaSyAp-4rbip43yTXSQi6u5j24nxmWvzxUivw",
+    authDomain: "rps-game-d993b.firebaseapp.com",
+    databaseURL: "https://rps-game-d993b.firebaseio.com",
+    projectId: "rps-game-d993b",
+    storageBucket: "rps-game-d993b.appspot.com",
+    messagingSenderId: "994168819528",
+    appId: "1:994168819528:web:1fde03218a2642cd"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+//reference to the database
+var database = firebase.database();
+
+//global variables
+var player1name;
+var player1move;
+var player1wins;
+var player1losses;
+
+var player2name;
+var player2move;
+var player2wins;
+var player2losses;
+
+
+
+// puts player 1 name to the database
+$('#nameButton').on('click',function(){
+    if database.ref().on("value", function(snapshot) {
+        player1name = $('#name').val();
+        database.ref().set({
+            Player1Name: player1name,
+            Player1Move: empty,
+            Player1Wins: 0,
+            Player1Losses: 0,
+        });  
+        //puts player1 name onto the page
+        $('#player1name').text(player1name);  
+    } else {
+        playzer2name = $('#name').val();
+        database.ref().set({
+            Player2Name: player2name,
+            Player2Move: empty,
+            Player2Wins: 0,
+            Player2Losses: 0,
+        });
+    }   
+});
+
+
+
+
